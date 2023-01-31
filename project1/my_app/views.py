54a537a085082fa675c1202e7528132ce16516f0
Arman Urazov <aurazov@myseneca.ca>
2023-01-31 03:43:43 +0100

    some changes

---
+++ project1/my_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def example_view(request):
+    return render(request, 'my_app/example.html')
+
+def variable(request):
+    my_var = {'first_name': 'Arman', 'last_name':'Arman',
+    'some_list':[1,2,3], 'some_dict': {'key1': 'value1','key2': 'value2','key3': 'value3'}}
+    return render(request, 'my_app/variable.html', context = my_var)
